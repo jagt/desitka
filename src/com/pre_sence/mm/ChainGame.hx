fc419bb5882704d303124ba6b680a285ebca92d7
jagt <jagt@live.com>
2012-09-09 23:56:26 +0800

    implement SumTiles array.

--- src/com/pre_sence/mm/ChainGame.hx
+++ src/com/pre_sence/mm/ChainGame.hx
@@ -6,15 +6,42 @@ import Tile.State;
  * ...
  * @author lilo
  */
-
+class SummedTiles
+{
+	public var arr:Array<Tile>;
+	
+	public function new() {
+		arr = new Array<Tile>();
+	}
+	
+	public function next_to(target:Array<Tile>):Bool {
+		for (lhs in arr) {
+			for (rhs in target) {
+				if (lhs.next_to(rhs)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	public function push_arr(target:Array<Tile>):Void {
+		for (tile in target) {
+			arr.push(tile);
+			tile.state = State.Summed;
+		}
+	}
+}
+ 
+ 
 class ChainGame extends Game
 {
-	public var summed:Array<Tile>;
+	public var summed:Array<SummedTiles>;
 	
 	public function new() 
 	{
 		super();
-		summed = new Array<Tile>();
+		summed = new Array<SummedTiles>();
 	}
 	
 	override private function clear_selected():Void 
@@ -22,9 +49,19 @@ class ChainGame extends Game
 		Auxi.assert(selected.is_full(), "clear on not full");
 		var modded = selected.sum() % Game.SUM;
 		if (modded == 0) {
-			for (tile in selected.arr) {
-				summed.push(tile);
-				tile.state = State.Summed;
+			var found_next_to = false;
+			for (sumtile in summed) {
+				if (sumtile.next_to(selected.arr)) {
+					sumtile.push_arr(selected.arr);
+					found_next_to = true;
+				}
+				break;
+			}
+			if (!found_next_to) {
+				var sumtile = new SummedTiles();
+				sumtile.push_arr(selected.arr);
+				summed.push(sumtile);
+				trace("new sumtile");
 			}
 		} else {
 			for (ix in 0...selected.length-1) {
